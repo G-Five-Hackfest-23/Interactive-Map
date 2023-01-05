7546329cbf508514d85f59f3e6021eaa31c0cc92
Aditya Kumar Kasaudhan <akasaudhan02@gmail.com>
2023-01-05 22:36:40 +0530

    creating signin

--- app/src/main/java/com/gfive/gpx/SplashScreen.kt
+++ app/src/main/java/com/gfive/gpx/SplashScreen.kt
@@ -1,11 +1,20 @@
 package com.gfive.gpx
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },2000)
+
     }
 }
